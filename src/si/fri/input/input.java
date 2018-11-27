bde590bc28a98fe1a157033e188a5a2ff68411ac
Katja <kk9905@student.uni-lj.si>
2018-11-27 20:10:53 +0100

    input (tko kt je v unem tutorialu)

---
+++ src/si/fri/input/input.java
@@ -0,0 +1,31 @@
+package si.fri.input;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import java.io.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class input extends GLFWKeyCallback {
+
+    public static boolean[] keys = new boolean[65536];
+
+    @Override
+    public void invoke(long window, int key, int scancode, int action, int mods) {
+        // TODO Auto-generated method stub
+        keys[key] = action != GLFW_RELEASE;
+    }
+
+    // boolean method that returns true if a given key
+    // is pressed.
+    public static boolean isKeyDown(int keycode) {
+        return keys[keycode];
+    }
+
+
+}
