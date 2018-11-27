a3470eb14bcec7db82836b72089c272e87c521c0
Katja <kk9905@student.uni-lj.si>
2018-11-27 20:34:47 +0100

    funkcije za pogled kamere

--- src/si/fri/rgti/Player.java
+++ src/si/fri/rgti/Player.java
@@ -15,4 +15,23 @@ public class Player
     public void update() {
 
     }
+
+
+    public static Matrix4f transformacijaKoordinat(double[] koordKamere, double d) {
+        Matrix4f matrikaPogleda = new Matrix4f();
+        Matrix4f translacijaKamere = translate(-koordKamere[0], -koordKamere[1], -koordKamere[2]);
+        Matrix4f perspektivaKamere = perspective(d);
+        Matrix4f.mul(matrikaPogleda, perspektivaKamere, translacijaKamere);
+        return matrikaPogleda;
+    }
+
+    public static Vector4f perspective(double d) {
+        Vector4f p = new Vector4f(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1/d, 0, 0, 0, 0);
+        return p;
+    }
+
+    public static Vector4f translate(double dx, double dy, double dz) {
+        Vector4f p = new Vector4f(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, dx, dy, dz, 1);
+    }
+
 }
