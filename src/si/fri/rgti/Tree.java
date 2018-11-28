0d5fb79b32ba7e44055ddf5b49a605033e4b3b05
Katja <kk9905@student.uni-lj.si>
2018-11-28 21:24:14 +0100

    dodan class za drevo

---
+++ src/si/fri/rgti/Tree.java
@@ -0,0 +1,25 @@
+package si.fri.rgti;
+
+public class Tree
+        extends GameObject
+        implements Interactable {
+
+    /* TODO
+    members: color, poison bool, clipping box?
+    methods: inherited
+     */
+
+    public double color;
+    public boolean poison;
+
+
+    public Tree() {
+        // TODO
+    }
+
+    @Override
+    public void Interact(Player player) {
+        // TODO
+    }
+
+}
