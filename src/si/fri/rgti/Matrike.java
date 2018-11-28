96ca44fae9b26715e3920062f4a8d9b734c3d756
Katja <kk9905@student.uni-lj.si>
2018-11-28 19:22:05 +0100

    matrike

---
+++ src/si/fri/rgti/Matrike.java
@@ -0,0 +1,53 @@
+package si.fri.rgti;
+
+public class Matrike {
+
+
+    // Setup projection matrix
+    public Matrix4f projectionMat(Matrix4f mat, double left, double right, double bottom, double top, double near, double far) {
+        Matrix4f projectionMatrix = mat.perspective(left, right, bottom, top, near, far);
+        return projectionMatrix;
+    }
+
+    // translating (moving) our view matrix over a certain vector
+    public Matrix4f translateMat(Vector3f vect, Matrix4f mat) {
+        Matrix4f.translate(vect, mat, mat);
+        return mat;
+    }
+
+    public Matrix4f scale(Vector3f vect, Matrix4f mat) {
+        Matrix4f.translate(vect, mat, mat);
+        return mat;
+    }
+
+    public Matrix4f rotateX(double angle, Matrix4f mat) {
+        Matrix4f.rotate(Math.toRadians(angle), new Vector3f(1, 0, 0), mat, mat);
+        return mat;
+    }
+
+    public Matrix4f rotateY(double angle, Matrix4f mat) {
+        Matrix4f.rotate(Math.toRadians(angle), new Vector3f(0, 1, 0), mat, mat);
+        return mat;
+    }
+
+    public Matrix4f rotateZ(double angle, Matrix4f mat) {
+        Matrix4f.rotate(Math.toRadians(angle), new Vector3f(0, 0, 1), mat, mat);
+        return mat;
+    }
+
+
+    public Matrix4f modelMatrix(Matrix4f modelMatrix, ) {
+        // model matrix, this has to be done in a specific order: scale, translate and rotate.
+        Matrix4f.scale(modelScale, modelMatrix, modelMatrix);
+        Matrix4f.translate(modelPos, modelMatrix, modelMatrix);
+        Matrix4f.rotate(this.degreesToRadians(modelAngle.z), new Vector3f(0, 0, 1),
+                modelMatrix, modelMatrix);
+        Matrix4f.rotate(this.degreesToRadians(modelAngle.y), new Vector3f(0, 1, 0),
+                modelMatrix, modelMatrix);
+        Matrix4f.rotate(this.degreesToRadians(modelAngle.x), new Vector3f(1, 0, 0),
+                modelMatrix, modelMatrix);
+    }
+
+
+
+}
