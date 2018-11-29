675d15fe2f96197908a398653a8f5071449ca6f0
Katja <kk9905@student.uni-lj.si>
2018-11-29 16:56:04 +0100

    PNG decoder ne dela še, ker neznam importat

---
+++ src/si/fri/rgti/Tex.java
@@ -0,0 +1,31 @@
+package si.fri.rgti;
+
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import java.nio.ByteBuffer;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+public class Tex{
+
+    private int id;
+
+    public Tex(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
